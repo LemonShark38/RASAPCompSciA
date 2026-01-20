11fddd80fe99afbe1250a759ae4e40b0c665bc6e
LemonShark38 <131652620+LemonShark38@users.noreply.github.com>
2026-01-20 10:54:59 +0800

    Update Unit-2-Magpie-2.0-Activity-3 PartC BYLEMON.java
    small mistake
    
    Signed-off-by: LemonShark38 <131652620+LemonShark38@users.noreply.github.com>

--- Unit-2-Magpie-2.0-Activity-3 PartC BYLEMON.java
+++ Unit-2-Magpie-2.0-Activity-3 PartC BYLEMON.java
@@ -3,7 +3,7 @@ private String transformWouldYouLikeStatement(String statement)
 {
 	int pos = findPhrase(statement, "like to", 0);
     int endpos = findPhrase(statement, "with", pos) - 1;
-  	String Obj = statement.substring(pos + 7, endpos);
+  	String Obj = statement.substring(pos + 8, endpos);
     
 	return ("When would you like me to" + Obj + " with you?");
 }
